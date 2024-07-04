cfb53118c35b5eae225419fb809571602630c8cb
TRIPTI JAISWAL <122300791+triptijaiswall@users.noreply.github.com>
2024-07-04 14:42:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let display = document.getElementById('display');
+let currentNumber = '';
+let previousNumber = '';
+let operation = null;
+
+function appendNumber(number) {
+    currentNumber += number;
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentNumber === '') return;
+    if (previousNumber !== '') {
+        compute();
+    }
+    operation = op;
+    previousNumber = currentNumber;
+    currentNumber = '';
+}
+
+function compute() {
+    let computation;
+    const prev = parseFloat(previousNumber);
+    const current = parseFloat(currentNumber);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentNumber = computation;
+    operation = null;
+    previousNumber = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentNumber = '';
+    previousNumber = '';
+    operation = null;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.innerText = currentNumber;
+}
+
+  
